a34c7454f70b7abff3e4d3c763233de432d029d8
kr_mohit1511 <ak735927@gmail.com>
2021-04-03 08:30:41 +0530

    Changed Title

--- src/Heading.js
+++ src/Heading.js
@@ -6,7 +6,7 @@ export const Heading = () => {
       <div className="container textCenter" style={{justifyContent:"space-between"}}>
         <div class="row">
           <div class="col-xl-8 mx-2 mr-2">
-            <h2 class="section-title" style={{textAlign:"center"}}>Presenting, <span>Splash the unsplash</span>, view</h2>
+            <h2 class="section-title" style={{textAlign:"center"}}>Presenting, <span>Splash the unsplash </span>view</h2>
           </div>
         </div>
       </div>
